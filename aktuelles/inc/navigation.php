cf51ecf9e5b2baa5ea518950daf02ac1caed6cda
Daniel Fritz <fritzdan@web.de>
2017-01-12 20:22:36 +0100

    Umstellung auf php zur besseren Modularisierung begonnen.
    Weitere Formatierungs-Verbesserungen.

---
+++ aktuelles/inc/navigation.php
@@ -0,0 +1,17 @@
+<!-- /aktuelles/navigation.php -->
+
+<table id="nav" cellpadding="0" cellspacing="0" width="190" border="0" style="">
+	<tbody>
+
+		<tr>
+			<td>
+				<a href="../aktuelles/index.html"><img src="leer.gif" width="15" height="1" border="0"><img src="../btn/pfeil1.gif" width="10" height="7" border="0"> Aktuelles</a>
+				<a href="next.html"><img src="leer.gif" width="15" height="1" border="0">&nbsp;&nbsp;&nbsp;&nbsp;- Nächstes Konzert &nbsp;&nbsp;</a>
+				<a href="konzerte.html"><img src="leer.gif" width="15" height="1" border="0">&nbsp;&nbsp;&nbsp;&nbsp;- Konzerte &nbsp;&nbsp;</a>
+				<a href="termine.html"><img src="leer.gif" width="15" height="1" border="0">&nbsp;&nbsp;&nbsp;&nbsp;- Terminübersicht &nbsp;&nbsp;&nbsp;</a>
+				<a href="#"></a>
+			</td>
+		</tr>
+
+	</tbody>
+</table>
--- aktuelles/konzerte.php
+++ aktuelles/konzerte.php
@@ -1,5 +1,5 @@
 <!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
-<!-- /aktuelles/konzerte.html -->
+<!-- /aktuelles/konzerte.php -->
 
 <html>
 	<head>
@@ -46,20 +46,9 @@
 	</table>
 
 	<!-- Navigation -->
-	<table id="nav" cellpadding="0" cellspacing="0" width="190" border="0" style="">
-	<tbody>
-
-		<tr>
-			<td><a href="../aktuelles/index.html"><img src="leer.gif" width="15" height="1" border="0"><img src="../btn/pfeil1.gif" width="10" height="7" border="0"> Aktuelles</a>
-			<a href="next.html"><img src="leer.gif" width="15" height="1" border="0">&nbsp;&nbsp;&nbsp;&nbsp;- Nächstes Konzert&nbsp;&nbsp;</a>
-			<a href="konzerte.html"><img src="leer.gif" width="15" height="1" border="0">&nbsp;&nbsp;&nbsp;&nbsp;- Konzerte &nbsp;&nbsp;</a>
-			<a href="termine.html"><img src="leer.gif" width="15" height="1" border="0">&nbsp;&nbsp;&nbsp;&nbsp;- Terminübersicht&nbsp;&nbsp;&nbsp;</a>
-			<a href="#"></a></td>
-		</tr>
-
-	</tbody>
-	</table>
-
+	<?php
+		include './inc/navigation.php'
+	?>
 
 	<!-- Inhaltsebene -->
 
