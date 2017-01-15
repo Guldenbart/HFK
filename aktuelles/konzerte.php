ed317baedff5ef6b9f36b7a29dbfa1b83eda15a8
Daniel Fritz <fritzdan@web.de>
2017-01-15 10:22:06 +0100

    jetzt tatsächlich aktuelles/konzerte.php vollständig überarbeitet ;)

--- aktuelles/konzerte.php
+++ aktuelles/konzerte.php
@@ -16,21 +16,9 @@
 
 	
 	<!-- Kopbereich -->
-	<table bgcolor="darkgray" width="100%" cellpadding="0" cellspacing="0" border="0">
-
-	<tr>
-		<td colspan="3"><img src="leer.gif" width="1" height="10" border="0"></td>
-	</tr>
-	<tr>
-		<td background="../start/gelb.gif"><img src="../start/gelb.gif" width="1" height="81" border="0"></td>
-		<td width="771" align="center"><img src="../start/hg_oben.jpg" width="771" height="81" border="0"></td>
-		<td background="../start/blau.gif"><img src="../start/blau.gif" width="1" height="81" border="0"></td>
-	</tr>
-	<tr>
-		<td colspan="3"><img src="leer.gif" width="1" height="10" border="0"></td>
-	</tr>
-
-	</table>
+	<?php
+		include '../inc/kopfbereich.php'
+	?>
 
 	
 	<!-- Kontaktleiste -->
